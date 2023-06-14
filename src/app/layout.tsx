3fe955537c7ad8308be4c908be7c96ff5f2800f2
LimjeongSik <ol37@naver.com>
2023-06-14 23:07:37 +0900

    style : styled-components ssr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import StyledComponentsRegistry from "@/lib/styledRegistry";
+import { GlobalStyle } from "@/styles/GlobalStyle";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +16,12 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <StyledComponentsRegistry>
+                    <GlobalStyle />
+                    {children}
+                </StyledComponentsRegistry>
+            </body>
         </html>
     );
 }
