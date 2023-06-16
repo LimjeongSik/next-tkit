393eaebeac0c2af578e0a5e532b8250119e892c8
LimjeongSik <ol37@naver.com>
2023-06-16 20:27:09 +0900

    feature : axios set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-    return <div>hola</div>;
+export default async function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
 }
